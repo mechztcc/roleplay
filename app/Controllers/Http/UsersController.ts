3c86b6a7ab9115a954e874db156b051331a944b1
mechztcc <dojathan@gmail.com>
2022-04-20 13:10:05 -0300

    feat: created User on UsersController

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,9 +1,11 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User';
 
 export default class UsersController {
   public async store({ response, request }: HttpContextContract) {
     const payload = request.only(['email', 'username', 'password', 'avatar']);
 
-
+		const user = await User.create(payload);
+		return user;
   }
 }
