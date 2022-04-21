872794caea45dc683b60e600f030b3a93a335ca8
mechztcc <dojathan@gmail.com>
2022-04-21 11:01:22 -0300

    feat: created PasswordsController

---
+++ tests/functional/users/password.spec.ts
@@ -0,0 +1,16 @@
+import { test } from '@japa/runner'
+import { UserFactory } from 'Database/factories'
+
+test.group('Users password', () => {
+  test('It should send a email with forgot password instructions', async ({ client }) => {
+    const user = await UserFactory.create()
+
+    const response = await client.post('/forgot-password').json({
+      email: user.email,
+      resetPasswordUrl: 'url',
+    });
+
+    response.assertStatus(204);
+    
+  })
+})
