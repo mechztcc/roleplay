7b1871d6a94359299d4f348e53a4609a6260a625
mechztcc <dojathan@gmail.com>
2022-04-21 12:04:14 -0300

    feat: send html when forgor password

--- app/Controllers/Http/PasswordsController.ts
+++ app/Controllers/Http/PasswordsController.ts
@@ -5,12 +5,12 @@ export default class PasswordsController {
   public async forgot({ request, response }: HttpContextContract) {
     const email = request.only(['email']) as unknown as string // ??? whats is it!!!
 
-    const msg = await Mail.sendLater((message) => {
+    const msg = await Mail.send((message) => {
       message
         .from('no-replay@roleplay.com')
         .to(email)
         .subject('Roleplay: Recuperação de senha')
-        .text('Clique no linik abaixo apra redefinir a sua senha.')
+        .htmlView('forgotpassword')
     })
     return response.noContent()
   }
