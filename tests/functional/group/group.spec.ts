cbbf56fe1b5e8243c6a415018996848b8ad8760d
mechztcc <dojathan@gmail.com>
2022-04-22 16:58:19 -0300

    fix: update create group test

--- tests/functional/group/group.spec.ts
+++ tests/functional/group/group.spec.ts
@@ -1,9 +1,23 @@
 import { test } from '@japa/runner'
+import { UserFactory } from 'Database/factories'
 
 test.group('Group group', () => {
   test('It should be create a group with successfully', async ({ client, assert}) => {
 
-    const response = await client.post('/groups').json({})
+    const user = await UserFactory.create();
+    
+    const groupPayload = {
+      name: 'test',
+      description: 'test',
+      schedule: 'test',
+      location: 'test',
+      chronic: 'test',
+      master: user.id
+    }
+
+    const response = await client.post('/groups').json(groupPayload)
+
+    const body = response.body()
 
     response.assertStatus(201)
   })
