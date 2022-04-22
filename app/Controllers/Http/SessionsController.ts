64cb07ee4ee934082d53002d4c08166671dfa707
mechztcc <dojathan@gmail.com>
2022-04-22 10:27:07 -0300

    feat: added adonis auth

--- app/Controllers/Http/SessionsController.ts
+++ app/Controllers/Http/SessionsController.ts
@@ -1,7 +1,15 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+import CreateSessionValidator from 'App/Validators/CreateSessionValidator'
 
 export default class SessionsController {
-  public async store({ request, response }: HttpContextContract) {
-      return response.noContent();
+  public async store({ request, response, auth }: HttpContextContract) {
+    const payload = await request.validate(CreateSessionValidator)
+
+    const user = await User.findByOrFail('email', payload.email)
+
+    await auth.attempt(payload.email, payload.password)
+
+    return response.noContent()
   }
 }
