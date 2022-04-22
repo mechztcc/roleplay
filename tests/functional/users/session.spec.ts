1480d743dbb66dc092975acb99a4d0d482b82408
mechztcc <dojathan@gmail.com>
2022-04-22 10:38:05 -0300

    feat: created test to validate when password has a invalid format when create session

--- tests/functional/users/session.spec.ts
+++ tests/functional/users/session.spec.ts
@@ -10,11 +10,24 @@ test.group('Users sessions', () => {
       .post('/sessions')
       .json({ email: user.email, password: plainPassword })
 
-    const body = response.body()  
+    const body = response.body()
 
     response.assertStatus(201)
     assert.equal(body.user.id, user.id, 'User undefined')
   })
 
-  // test('Its should')
+  test('Its should return 422 when password has a invalid format', async ({ client, assert }) => {
+    const plainPassword = '123'
+    const user = await UserFactory.merge({ password: plainPassword }).create()
+
+    const response = await client
+      .post('/sessions')
+      .json({ email: user.email, password: plainPassword })
+      
+    response.assertBodyContains({
+      code: 'BAD_REQUEST',
+      status: 422,
+      message: 'E_VALIDATION_FAILURE: Validation Exception',
+    })
+  })
 })
