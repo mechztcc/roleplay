520b9c100d3ebde9fa2dbc8318b664406d4abedb
mechztcc <dojathan@gmail.com>
2022-04-22 10:17:22 -0300

    fix: update test to verify the returns on authenticate user

--- tests/functional/users/session.spec.ts
+++ tests/functional/users/session.spec.ts
@@ -1,10 +1,17 @@
 import { test } from '@japa/runner'
+import { UserFactory } from 'Database/factories'
 
 test.group('Users sessions', () => {
-  test('It should authenticate an user', async({ client, assert }) => {
+  test('It should authenticate an user', async ({ client, assert }) => {
+    const plainPassword = 'test'
+    const user = await UserFactory.merge({ password: plainPassword }).create()
 
-    const response = await client.post('/sessions')
+    const response = await client
+      .post('/sessions')
+      .json({ email: user.email, password: plainPassword })
 
+    const body = response.body()  
     response.assertStatus(201)
+    assert.equal(body.id, user.id, 'User undefined')
   })
 })
