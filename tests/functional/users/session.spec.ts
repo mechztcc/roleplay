f58ba060392f15d38a39b9528f908cfe2dd2f061
mechztcc <dojathan@gmail.com>
2022-04-22 10:41:58 -0300

    feat: created test to verify if token as generated when create session

--- tests/functional/users/session.spec.ts
+++ tests/functional/users/session.spec.ts
@@ -23,11 +23,26 @@ test.group('Users sessions', () => {
     const response = await client
       .post('/sessions')
       .json({ email: user.email, password: plainPassword })
-      
+
     response.assertBodyContains({
       code: 'BAD_REQUEST',
       status: 422,
       message: 'E_VALIDATION_FAILURE: Validation Exception',
     })
+  })
+
+  test('Its should return an api token when create a session', async ({ client, assert }) => {
+    const plainPassword = '123456'
+    const user = await UserFactory.merge({ password: plainPassword }).create()
+
+    const response = await client
+      .post('/sessions')
+      .json({ email: user.email, password: plainPassword })
+
+    const body = response.body()
+
+    response.assertStatus(201)
+    assert.equal(body.user.id, user.id, 'User undefined')
+    assert.isDefined(body.token, 'User token not generated')
   })
 })
