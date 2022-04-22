4dd9da3df7d1f798342656b41dc090e96a534f33
mechztcc <dojathan@gmail.com>
2022-04-22 10:46:16 -0300

    feat: generated token with successfully when create session

--- app/Controllers/Http/SessionsController.ts
+++ app/Controllers/Http/SessionsController.ts
@@ -1,15 +1,14 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import User from 'App/Models/User'
 import CreateSessionValidator from 'App/Validators/CreateSessionValidator'
 
 export default class SessionsController {
   public async store({ request, response, auth }: HttpContextContract) {
     const payload = await request.validate(CreateSessionValidator)
 
-    const user = await User.findByOrFail('email', payload.email)
+    const token = await auth
+      .use('api')
+      .attempt(payload.email, payload.password, { expiresIn: '2hours' })
 
-    await auth.attempt(payload.email, payload.password)
-
-    return response.created({ user: auth.user })
+    return response.created({ user: auth.user, token: token })
   }
 }
