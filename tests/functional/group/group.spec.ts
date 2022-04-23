42e51f9cb0992038796882b5f638b8b9b6366772
mechztcc <dojathan@gmail.com>
2022-04-23 09:50:24 -0300

    feat: defined test when required data its not proved during create group

--- tests/functional/group/group.spec.ts
+++ tests/functional/group/group.spec.ts
@@ -2,24 +2,34 @@ import { test } from '@japa/runner'
 import { UserFactory } from 'Database/factories'
 
 test.group('Group group', () => {
-  test('It should be create a group with successfully', async ({ client, assert}) => {
+  test('It should be create a group with successfully', async ({ client, assert }) => {
+    const user = await UserFactory.create()
 
-    const user = await UserFactory.create();
-    
     const groupPayload = {
       name: 'test',
       description: 'test',
       schedule: 'test',
       location: 'test',
       chronic: 'test',
-      master: user.id
+      master: user.id,
     }
 
     const response = await client.post('/groups').json(groupPayload)
 
-    const body = response.body()
-  
     response.assertStatus(201)
     response.assertBodyContains({ master: user.id, id: 1 })
+  })
+
+  test('It should return 422 when required data its not provided', async ({ client }) => {
+    const response = await client.post('/groups').json({})
+
+    response.assertStatus(422)
+
+    response.assertBodyContains({
+      code: 'BAD_REQUEST',
+      status: 422,
+      message: 'E_VALIDATION_FAILURE: Validation Exception',
+      errors: [],
+    })
   })
 })
