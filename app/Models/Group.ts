91f75be978b31d57760a27ed6727c6af699921d7
mechztcc <dojathan@gmail.com>
2022-04-23 09:29:52 -0300

    feat: defined relations between Group and User

--- app/Models/Group.ts
+++ app/Models/Group.ts
@@ -1,9 +1,33 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, belongsTo, BelongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
 
 export default class Group extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public description: string
+
+  @column()
+  public schedule: string
+
+  @column()
+  public location: string
+
+  @column()
+  public chronic: string
+
+  @column()
+  public master: number
+
+  @belongsTo(() => User, {
+    foreignKey: 'master',
+  })
+  public masterUser: BelongsTo<typeof User>
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
