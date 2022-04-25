a47b10684618c2333dc4618dc6d1a2fb9907a2fb
mechztcc <dojathan@gmail.com>
2022-04-25 11:50:42 -0300

    feat: created test to verify when user try to span group request

--- tests/functional/group/group_request.spec.ts
+++ tests/functional/group/group_request.spec.ts
@@ -28,4 +28,29 @@ test.group('Group request', () => {
     assert.exists(responseBody.status, 'Status not found')  
     response.assertStatus(201)
   })
+
+  test('It should return 409 when group request already exists', async ({ client, assert }) => {
+    const plainPassword = '123456'
+    const user = await UserFactory.merge({ password: plainPassword }).create()
+
+    const login = await client
+      .post('/sessions')
+      .json({ email: user.email, password: plainPassword })
+
+    const body = login.body()
+    login.assertStatus(201)
+
+    const master = await UserFactory.create()
+    const group = await GroupFactory.merge({ master: master.id }).create()
+
+    await client
+      .post(`/groups/${group.id}/requests`)
+      .header('Authorization', `Bearer ${body.token.token}`)
+
+    const response = await client
+      .post(`/groups/${group.id}/requests`)
+      .header('Authorization', `Bearer ${body.token.token}`)
+
+    response.assertStatus(409)
+  })
 })
