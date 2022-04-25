838ba339c33da87e07144e7c16b0d8815f91f270
mechztcc <dojathan@gmail.com>
2022-04-25 10:40:43 -0300

    feat: added controller to group request

--- tests/functional/group/group_request.spec.ts
+++ tests/functional/group/group_request.spec.ts
@@ -3,7 +3,7 @@ import { test } from '@japa/runner'
 test.group('Group request', () => {
   test('It should create a group request', async ({ client, assert}) => {
 
-    const response = await client.post('/group/1/request')
+    const response = await client.post('/groups/1/requests')
 
     response.assertStatus(201)
   })
