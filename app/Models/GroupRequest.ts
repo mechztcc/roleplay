3c989312b0fe4c1e248054cdb5e70f12021f89f6
mechztcc <dojathan@gmail.com>
2022-04-25 11:01:34 -0300

    feat: defined group request model

--- app/Models/GroupRequest.ts
+++ app/Models/GroupRequest.ts
@@ -1,9 +1,32 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, belongsTo, BelongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
+import Group from './Group'
 
 export default class GroupRequest extends BaseModel {
+  public static table = 'groups_requests'
+
   @column({ isPrimary: true })
   public id: number
+
+  @column({ columnName: 'user_id' })
+  public userId: number
+
+  @column({ columnName: 'group_id' })
+  public groupId: number
+
+  @column()
+  public status: string
+
+  @belongsTo(() => User, {
+    foreignKey: 'userId',
+  })
+  public user: BelongsTo<typeof User>
+
+  @belongsTo(() => Group, {
+    foreignKey: 'groupId',
+  })
+  public group: BelongsTo<typeof Group>
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
