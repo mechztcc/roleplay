2081b8150f978cece83c3b0c6b79bceb0588ce62
mechztcc <dojathan@gmail.com>
2022-04-25 10:50:13 -0300

    fix: update test to create group request

--- tests/functional/group/group_request.spec.ts
+++ tests/functional/group/group_request.spec.ts
@@ -1,9 +1,13 @@
 import { test } from '@japa/runner'
+import { GroupFactory, UserFactory } from 'Database/factories'
 
 test.group('Group request', () => {
   test('It should create a group request', async ({ client, assert}) => {
 
-    const response = await client.post('/groups/1/requests')
+    const master = await UserFactory.create();
+    const group = await GroupFactory.merge({ master: master.id}).create()
+
+    const response = await client.post(`/groups/${group.id}/requests`)
 
     response.assertStatus(201)
   })
